293864639cec0139beea67045beb1b4d985c4854
toyiselateef <37558044+toyiselateef@users.noreply.github.com>
2021-01-14 06:44:50 +0100

    UpdateHomeController.cs
    I actually did nothing, just taking a tour

--- LearningSystemOA.Core/Controllers/HomeController.cs
+++ LearningSystemOA.Core/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+//StaySafe
 namespace LearningSystemOA.Core.Controllers
 {
     public class HomeController : Controller
